30dbd972d1798ddfefe254b0c9d4b301069e23f6
colladou@uci.edu <colladou@uci.edu>
2018-02-09 15:26:24 -0800

    updated file to accept tag 'other' instead of just signal and bg

--- flatten_hdf5.py
+++ flatten_hdf5.py
@@ -58,6 +58,8 @@ if sys.argv[1] == 'signal':
     signal = True
 elif sys.argv[1] == 'bg':
     signal = False
+elif sys.argv[1] == 'other':
+    signal = False
 else:
     assert 1==0, "please specify signal or bg"
 
@@ -68,32 +70,18 @@ else:
     new_file_dataset_name = "temporary_flattened_data_bg.h5"
     round_down = 10.0 
 
-feature_names = [u'clusters', u'jets', u'subjet1', u'subjet2', u'tracks']
+feature_names = [u'fat_jet', u'subjet1', u'subjet2', u'subjet1_tracks', u'subjet2_tracks']
 
 # This list can contain the names of many h5 files and it will merge them into one.
-file_list_s = ['d301488_j1.h5', 'd301489_j2.h5',
-                   'd301490_j3.h5', 'd301491_j4.h5',
-                   'd301492_j5.h5', 'd301493_j6.h5',
-                   'd301494_j7.h5', 'd301495_j8.h5',
-                   'd301496_j9.h5', 'd301497_j10.h5',
-                   'd301498_j11.h5', 'd301499_j12.h5',
-                   'd301500_j13.h5', 'd301501_j14.h5',
-                   'd301502_j15.h5', 'd301503_j16.h5',
-                   'd301504_j17.h5', 'd301505_j18.h5',
-                   'd301506_j19.h5', 'd301507_j20.h5',]
-file_list_bg = [#'d361020_j26.h5', 'd361021_j27.h5',
-                    'd361022_j28.h5', 'd361023_j29.h5',
-                    'd361024_j30.h5', 'd361025_j31.h5',
-                    'd361026_j32.h5', 'd361027_j33.h5',
-                    'd361028_j34.h5', 'd361029_j35.h5',
-                    'd361030_j36.h5', 'd361031_j37.h5',
-                    'd361032_j38.h5',]  
-              
+file_list_s = []
+file_list_bg = []
              
-if signal:
+if sys.argv[1] == 'signal':
     file_list = file_list_s
-else:
+elif sys.argv[1] == 'bg':
     file_list = file_list_bg
+elif sys.argv[1] == 'other':
+    file_list = ['output.h5']
 
 f_names = []
 for name in file_list:
@@ -123,13 +111,12 @@ for feature_name in feature_names:
         col_names = category_names[feature_name]
         assert data is not None
         N = int(total_samples)
-        new_sizes = {u'clusters': (N, 5, 60),
-                 u'jets': (N, 11),
-                 u'subjet1': (N, 40),
-                 u'subjet2': (N, 40),
-                 u'subjet3': (N, 40),
-                 u'tracks': (N, 29, 60),
-                }
+        new_sizes = {u'fat_jet': (N, 17),
+                     u'subjet1': (N, 46),
+                     u'subjet2': (N, 46),
+                     u'subjet1_tracks': (N, 21, 10), # 10 tracks each with 21 variables
+                     u'subjet2_tracks': (N, 21, 10),
+                    }
 
         # Create the new empty dataset
         if start == 0:
